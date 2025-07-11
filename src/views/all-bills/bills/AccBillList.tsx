36987a75171b63b9a15b048823eddb7ad15135f0
VysakhSdev <vysakhs807@gmail.com>
2025-07-11 17:17:49 +0530

    new  changes

--- src/views/all-bills/bills/AccBillList.tsx
+++ src/views/all-bills/bills/AccBillList.tsx
@@ -1,18 +1,24 @@
 import AdaptableCard from '@/components/shared/AdaptableCard'
 import BillTable from './components/BillTables'
 import BillTableTools from './components/BillTableTools'
+import { useState } from 'react'
 
 
 
 
 const AccBillList = () => {
+    const [selectedDropdownMonth, setSelectedDropdownMonth] = useState('')
+
+    const handleDropdownSelect = (value: string) => {
+        setSelectedDropdownMonth(value)
+    }
     return (
         <AdaptableCard className="h-full" bodyClass="h-full">
             <div className="lg:flex items-center justify-between mb-4">
-                <h3 className="mb-4 lg:mb-0">Accommodation Bills</h3>
+                <h3 className="mb-4 lg:mb-0">Accommodation Bills {selectedDropdownMonth}</h3>
                 <BillTableTools to="/app/new-acc-bill" title="Add Bill" />
             </div>
-            <BillTable />
+            <BillTable onDropdownSelect={handleDropdownSelect} />
         </AdaptableCard>
     )
 }
