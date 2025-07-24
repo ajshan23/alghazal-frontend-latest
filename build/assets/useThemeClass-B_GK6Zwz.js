18f7c8952c50bb3c6951bda3796d4e15f3d2a717
ajshan23 <ajmalshahan23@gmail.com>
2025-07-24 15:49:47 +0530

    final relase 1.0

--- build/assets/useThemeClass-B_GK6Zwz.js
+++
@@ -1,1 +0,0 @@
-import{ah as t}from"./index-3DLZI4eB.js";function n(){const{themeColor:r,primaryColorLevel:o}=t(),e=`${r}-${o}`;return{ringTheme:`ring-${e}`,borderTheme:`border-${e}`,bgTheme:`bg-${e}`,textTheme:`text-${e}`}}export{n as u};
